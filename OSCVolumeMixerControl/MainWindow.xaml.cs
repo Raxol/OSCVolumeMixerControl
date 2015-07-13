d186a769b7f739deab504abce91af28fa0193993
Maximilian Siekmann <max.raxol@gmail.com>
2015-07-13 03:38:36 +0200

    added raw concept

--- OSCVolumeMixerControl/MainWindow.xaml.cs
+++ OSCVolumeMixerControl/MainWindow.xaml.cs
@@ -12,17 +12,95 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Net;
+using Bespoke.Common;
+using Bespoke.Common.Osc;
+using CoreAudio;
 
 namespace OSCVolumeMixerControl
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+
     public partial class MainWindow : Window
     {
+        private static readonly int Port = 8000;
+        private static readonly string AliveMethod = "/osctest/alive";
+        private static readonly string TestMethod = "/osctest/test";
+
+        private static int sBundlesReceivedCount;
+        private static int sMessagesReceivedCount;
+
+        private static MMDevice device;
+        
         public MainWindow()
         {
             InitializeComponent();
+
+            MMDeviceEnumerator DevEnum = new MMDeviceEnumerator();
+            device = DevEnum.GetDefaultAudioEndpoint(EDataFlow.eRender, ERole.eMultimedia);
+
+            OscServer oscServer;
+            oscServer = new OscServer(TransportType.Udp, IPAddress.Any, Port);
+
+            oscServer.FilterRegisteredMethods = false;
+            oscServer.RegisterMethod(AliveMethod);
+            oscServer.RegisterMethod(TestMethod);
+            oscServer.BundleReceived += new EventHandler<OscBundleReceivedEventArgs>(oscServer_BundleReceived);
+            oscServer.MessageReceived += new EventHandler<OscMessageReceivedEventArgs>(oscServer_MessageReceived);
+            oscServer.ReceiveErrored += new EventHandler<Bespoke.Common.ExceptionEventArgs>(oscServer_ReceiveErrored);
+            oscServer.ConsumeParsingExceptions = false;
+
+            oscServer.Start();
+        }
+
+        private static void oscServer_BundleReceived(object sender, OscBundleReceivedEventArgs e)
+        {
+            sBundlesReceivedCount++;
+
+            OscBundle bundle = e.Bundle;
+            Console.WriteLine(string.Format("\nBundle Received [{0}:{1}]: Nested Bundles: {2} Nested Messages: {3}", bundle.SourceEndPoint.Address, bundle.TimeStamp, bundle.Bundles.Count, bundle.Messages.Count));
+            Console.WriteLine("Total Bundles Received: {0}", sBundlesReceivedCount);
+        }
+
+        private static void oscServer_MessageReceived(object sender, OscMessageReceivedEventArgs e)
+        {
+            sMessagesReceivedCount++;
+
+            OscMessage message = e.Message;
+
+            Console.WriteLine(string.Format("\nMessage Received [{0}]: {1}", message.SourceEndPoint.Address, message.Address));
+            Console.WriteLine(string.Format("Message contains {0} objects.", message.Data.Count));
+
+            for (int i = 0; i < message.Data.Count; i++)
+            {
+                string dataString;
+
+                if (message.Data[i] == null)
+                {
+                    dataString = "Nil";
+                }
+                else
+                {
+                    dataString = (message.Data[i] is byte[] ? BitConverter.ToString((byte[])message.Data[i]) : message.Data[i].ToString());
+                    for (int j = 0; j < device.AudioSessionManager2.Sessions.Count; j++)
+                    {
+                        if (device.AudioSessionManager2.Sessions[j].GetProcessID == 7068)
+                        {
+                            device.AudioSessionManager2.Sessions[j].SimpleAudioVolume.MasterVolume = Convert.ToSingle(dataString) / 100;
+                        }
+                    }
+                }
+                Console.WriteLine(string.Format("[{0}]: {1}", i, dataString));
+            }
+
+            Console.WriteLine("Total Messages Received: {0}", sMessagesReceivedCount);
+        }
+
+        private static void oscServer_ReceiveErrored(object sender, Bespoke.Common.ExceptionEventArgs e)
+        {
+            Console.WriteLine("Error during reception of packet: {0}", e.Exception.Message);
         }
     }
 }
